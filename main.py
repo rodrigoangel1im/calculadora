d44b3fc18e8733306406342be7eadfc9d283143f
Rodrigo Angelim <angelimr0drig01@gmail.com>
2026-02-25 14:02:07 -0300

    Criação dos codigos
    Criação da função de somar e subtrair, e da janela da calculadora

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
 import tkinter as tk
+def somar():
+    a = int(entrada1.get())
+    b = int(entrada2.get())
 
+    resultado = a + b
+
+    l_resultado.config(text=f"Resultado:{resultado}")
+
+def subtrair():
+    a = int(entrada1.get())
+    b = int(entrada2.get())
+
+    resultado = a - b
+
+    l_resultado.config(text=f"Resultado: {resultado}")
 janela = tk.Tk()
-janela.title("Minha primeira janela")
+janela.title("Calculadora Simples")
 
-label = tk.Label(janela, text="Olá, mundo !")
-label.pack()
+entrada1 = tk.Entry(janela)
+entrada1.pack()
+
+entrada2 = tk.Entry(janela)
+entrada2.pack()
+
+botao = tk.Button(janela, text="Somar", command=somar)
+botao.pack()
+
+botao_subtrair = tk.Button(janela, text="Subtrair", command=subtrair)
+botao_subtrair.pack()
+
+l_resultado = tk.Label(janela, text="Resultado:")
+l_resultado.pack()
+
 janela.mainloop()
